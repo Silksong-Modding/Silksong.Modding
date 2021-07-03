f132e96cc084f0749fd4c6c883c521d8d799b3ae
Mandar1jn <marijnkneppers@gmail.com>
2021-07-03 20:40:44 +0200

    added the missing documentation to LogLevel.cs

--- Assembly-CSharp/enums/LogLevel.cs
+++ Assembly-CSharp/enums/LogLevel.cs
@@ -1,11 +1,33 @@
 ﻿namespace Modding.enums
 {
+	/// <summary>
+	///		All possible level to log to the console
+	/// </summary>
 	public enum LogLevel : byte
 	{
+		/// <summary>
+		///		The info log level
+		/// </summary>
 		INFO,
+
+		/// <summary>
+		///		The warning log level
+		/// </summary>
 		WARNING,
+
+		/// <summary>
+		///		The error log level
+		/// </summary>
 		ERROR,
+
+		/// <summary>
+		///		The debug log level
+		/// </summary>
 		DEBUG,
+
+		/// <summary>
+		///		The fine log level
+		/// </summary>
 		FINE
 	}
 }
