f9fe5cdbe3486e767c0e4836572c6992a043a03a
Mandar1jn <marijnkneppers@gmail.com>
2021-07-03 21:26:45 +0200

    created a mod class implementing IMod

---
+++ Assembly-CSharp/Mod.cs
@@ -0,0 +1,100 @@
+﻿using Modding.enums;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Modding
+{
+	public class Mod : IMod
+	{
+		public string GetName()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<(string, string)> GetPreloadNames()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetVersion()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Initialize()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int LoadPriority()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Log(string message, LogLevel level)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Log(object message, LogLevel level)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogDebug(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogDebug(object message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogError(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogError(object message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogFine(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogFine(object message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogInfo(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogInfo(object message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogWarning(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void LogWarning(object message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
